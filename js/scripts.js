086127584fb378f9477fd8272a73ec096d6e082a
NenoyHeker <106822874+NenoyHeker@users.noreply.github.com>
2024-04-04 10:41:12 -0600

    Formulario
    Validación de formulario y estilos con bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,59 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var nombre = document.getElementById('name');
+var correo = document.getElementById('email');
+var telefono = document.getElementById('phone');
+var mensaje = document.getElementById('message');
+
+var er = document.getElementById('submitErrorMessage')
+
+// 
+// function handleSubmit() {
+    // var messageEr = [];
+    
+    // const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // const regexMessage = /^[a-zA-Z0-9-_ ]+$/;
+
+    // if(nombre.value === null || nombre.value === ''){
+        // messageEr.push('Error al enviar: Ingresa tu nombre');
+    // }
+    // if(correo.value === null || !regexEmail.test(correo.value)){
+        // messageEr.push('Error al enviar: Ingresa un correo valido');
+    // }
+    // if(telefono.value.lenght < 10 || telefono.value.lenght > 10){
+        // messageEr.push('Error al enviar: El número de telefono debe ser igual a 10 caracteres')
+    // }
+    // if(!regexMessage.test(mensaje.value)){
+        // messageEr.push('Error al enviar: Uso de caracteres especiales en el Mensaje')
+    // }
+
+    // error.innerHTML = messageEr.join(', ');
+    // return false;
+// }
+// 
+
+
+var form = document.getElementById('contactForm');
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        console.log("si furula");
+        
+        const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const regexMessage = /^[a-zA-Z0-9-_ ]+$/;
+
+        if(nombre.value === null || nombre.value === ''){
+            messageEr.push('Error al enviar: Ingresa tu nombre');
+        }
+        if(correo.value === null || !regexEmail.test(correo.value)){
+            messageEr.push('Error al enviar: Ingresa un correo valido');
+        }
+        if(telefono.value === null){
+            messageEr.push('Error al enviar: El número de telefono invalido')
+        }
+        if(!regexMessage.test(mensaje.value)){
+            messageEr.push('Error al enviar: Uso de caracteres especiales en el Mensaje')
+        }
+
+        error.innerHTML = messageEr.join(', ');
+        return false;
+        
+    }
+)
