ece730c379fa52635892f174cef4d5f96a4cd689
NenoyHeker <106822874+NenoyHeker@users.noreply.github.com>
2024-04-04 11:09:32 -0600

    Version final

--- js/scripts.js
+++ js/scripts.js
@@ -5,32 +5,6 @@ var mensaje = document.getElementById('message');
 
 var er = document.getElementById('submitErrorMessage')
 
-// 
-// function handleSubmit() {
-    // var messageEr = [];
-    
-    // const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // const regexMessage = /^[a-zA-Z0-9-_ ]+$/;
-
-    // if(nombre.value === null || nombre.value === ''){
-        // messageEr.push('Error al enviar: Ingresa tu nombre');
-    // }
-    // if(correo.value === null || !regexEmail.test(correo.value)){
-        // messageEr.push('Error al enviar: Ingresa un correo valido');
-    // }
-    // if(telefono.value.lenght < 10 || telefono.value.lenght > 10){
-        // messageEr.push('Error al enviar: El número de telefono debe ser igual a 10 caracteres')
-    // }
-    // if(!regexMessage.test(mensaje.value)){
-        // messageEr.push('Error al enviar: Uso de caracteres especiales en el Mensaje')
-    // }
-
-    // error.innerHTML = messageEr.join(', ');
-    // return false;
-// }
-// 
-
-
 var form = document.getElementById('contactForm');
     form.addEventListener('submit', function(event){
         event.preventDefault();
